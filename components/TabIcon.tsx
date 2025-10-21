5727b86cf7b9aaa682bc6216ff782d506ea0d038
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-10-21 17:27:21 -0500

    (feature): Init TabIcon

---
+++ components/TabIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const TabIcon = () => {
+  return (
+    <View>
+      <Text>TabIcon</Text>
+    </View>
+  )
+}
+
+export default TabIcon
